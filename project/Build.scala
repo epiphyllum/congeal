a52a85575f264ebd6f70c7e62e4ba9671ca771c3
John Sullivan <john.sullivan.mscs@gmail.com>
2013-05-30 11:27:52 -0400

    up version from 0.2.0-SNAPSHOT to 0.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "mscs.sullivan.john",
-    version := "0.2.0-SNAPSHOT",
+    version := "0.2.0",
     scalacOptions ++= Seq("-deprecation"),
     scalaVersion := "2.11.0-SNAPSHOT",
     scalaOrganization := "org.scala-lang.macro-paradise",
