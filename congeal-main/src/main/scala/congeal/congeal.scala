39b7936b0ddede47e1a36e4881e2c870fb56207f
John Sullivan <john.sullivan.mscs@gmail.com>
2013-06-01 11:03:06 -0400

    begin refactoring effort to allow for generalizing easyMock into mock

--- congeal-main/src/main/scala/congeal/congeal.scala
+++ congeal-main/src/main/scala/congeal/congeal.scala
@@ -14,41 +14,41 @@ package object congeal {
   /** Produces an API for the supplied type `A`.
     * @tparam A the base type to produce an API for
     */
-  type api[A] = macro ApiMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type api[A] = macro macroImpls.api[A]
 
   /** Produces a default implementation for the supplied type `A`.
     * @tparam A the base type to produce an implementation for
     */
-  type impl[A] = macro ImplMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type impl[A] = macro macroImpls.impl[A]
 
   /** Produces a component API for the supplied type `A`.
     * @tparam A the base type to produce a component API for
     */
-  type componentApi[A] = macro ComponentApiMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type componentApi[A] = macro macroImpls.componentApi[A]
 
   /** Produces a component implementation for the supplied type `A`.
     * @tparam A the base type to produce a component API for
     */
-  type componentImpl[A] = macro ComponentImplMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type componentImpl[A] = macro macroImpls.componentImpl[A]
 
   /** Indicates a dependency on the component for the supplied type `A`.
     * @tparam A the base type to indicate a dependency on
     */
-  type hasDependency[A] = macro HasDependencyMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type hasDependency[A] = macro macroImpls.hasDependency[A]
 
   /** Indicates a sub-component for the supplied type `A`.
     * @tparam A the base type of the sub-component
     */
-  type hasPart[A] = macro HasPartMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type hasPart[A] = macro macroImpls.hasPart[A]
 
   /** Indicates a replacement component for the supplied type `A`.
     * @tparam A the base type of the replacement component
     */
-  type standsInFor[A] = macro StandsInForMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type standsInFor[A] = macro macroImpls.standsInFor[A]
 
   /** Indicates an EasyMock replacement component for the supplied type `A`.
     * @tparam A the base type of the mock component
     */
-  type easyMock[A] = macro EasyMockMacroImpl.refToTopLevelClassDefEnsureSimple[A]
+  type easyMock[A] = macro macroImpls.easyMock[A]
 
 }
---
+++ congeal-main/src/main/scala/congeal/macroImpls.scala
@@ -0,0 +1,33 @@
+package congeal
+
+import language.experimental.macros
+
+import scala.reflect.macros.Context
+
+private[congeal] object macroImpls {
+  
+  def api[T: c.WeakTypeTag](c: Context): c.Tree =
+    ApiMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def impl[T: c.WeakTypeTag](c: Context): c.Tree =
+    ImplMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def componentApi[T: c.WeakTypeTag](c: Context): c.Tree =
+    ComponentApiMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def componentImpl[T: c.WeakTypeTag](c: Context): c.Tree =
+    ComponentImplMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def hasDependency[T: c.WeakTypeTag](c: Context): c.Tree =
+    HasDependencyMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def hasPart[T: c.WeakTypeTag](c: Context): c.Tree =
+    HasPartMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def standsInFor[T: c.WeakTypeTag](c: Context): c.Tree =
+    StandsInForMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+  def easyMock[T: c.WeakTypeTag](c: Context): c.Tree =
+    EasyMockMacroImpl.refToTopLevelClassDefEnsureSimple[T](c)
+
+}
