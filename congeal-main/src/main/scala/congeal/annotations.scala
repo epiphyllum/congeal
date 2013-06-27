f941e14bae4f98b6daef9a8564becfb01d5a9673
John Sullivan <john.sullivan.mscs@gmail.com>
2013-06-27 09:19:54 -0400

    promote annotations stash to a branch
    (before it gets lost)

---
+++ congeal-main/src/main/scala/congeal/annotations.scala
@@ -0,0 +1,65 @@
+package congeal
+
+import language.experimental.macros
+import scala.reflect.macros.AnnotationMacro
+import scala.reflect.macros.AnnotationContext
+
+/** Contains front ends for the congeal annotation macros.
+  *
+  * All macros require type `A` to be "simple", i.e., meet the following conditions:
+  * 
+  *   - is a trait
+  *   - is static (i.e., not a member of a method or trait. only objects all the way up.)
+  *   - no non-private[this] inner classes
+  *   - no members that have params or return types that derive from A
+  */
+package object annotations {
+
+  /** Produces an API for the supplied type `A`.
+    * @tparam A the base type to produce an API for
+    */
+  //annotation api = macro foo
+
+  final class api extends AnnotationMacro
+
+  /** Produces a default implementation for the supplied type `A`.
+    * @tparam A the base type to produce an implementation for
+    */
+  type impl[A] = macro macroImpls.impl[A]
+
+  /** Produces a component API for the supplied type `A`.
+    * @tparam A the base type to produce a component API for
+    */
+  type componentApi[A] = macro macroImpls.componentApi[A]
+
+  /** Produces a component implementation for the supplied type `A`.
+    * @tparam A the base type to produce a component API for
+    */
+  type componentImpl[A] = macro macroImpls.componentImpl[A]
+
+  /** Indicates a dependency on the component for the supplied type `A`.
+    * @tparam A the base type to indicate a dependency on
+    */
+  type hasDependency[A] = macro macroImpls.hasDependency[A]
+
+  /** Indicates a sub-component for the supplied type `A`.
+    * @tparam A the base type of the sub-component
+    */
+  type hasPart[A] = macro macroImpls.hasPart[A]
+
+  /** Indicates a private sub-component for the supplied type `A`.
+    * @tparam A the base type of the private sub-component
+    */
+  type hasPrivatePart[A] = macro macroImpls.hasPrivatePart[A]
+
+  /** Indicates a replacement component for the supplied type `A`.
+    * @tparam A the base type of the replacement component
+    */
+  type standsInFor[A] = macro macroImpls.standsInFor[A]
+
+  /** Indicates an EasyMock replacement component for the supplied type `A`.
+    * @tparam A the base type of the mock component
+    */
+  type easyMock[A] = macro macroImpls.easyMock[A]
+
+}
